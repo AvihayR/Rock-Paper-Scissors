6b6a12100c53431fb7ac70ce9e819ea6d4e0a88e
AvihayR <avihayrabi@gmail.com>
2022-08-26 15:04:16 -0700

    Add round() func

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
+//computer object - stores array named choice with all pc's possibilities
 let computer = {
     choice: ['rock','paper','scissors']
 };
 
-
+//getComputerChoice - a func that generates a random number, multiplies it by computer.choice length, and floors it to a round num
 function getComputerChoice(){
-    let computersChoice = Math.floor(Math.random() * computer.choice.length);
-    console.log(computer.choice[computersChoice])
+    let randomChoice = Math.floor(Math.random() * computer.choice.length);
+    console.log(computer.choice[randomChoice])
+    return (computer.choice[randomChoice]);
 }
 
-getComputerChoice()
+//round - a func that takes two params - playerSelection & computerSelection,
+    //prompts an input pop-up window for the user then converts input into lowerCase. 
+    //checks to see who's the winner and returns the string that declare if player won or lost.
+function round(playerSelection,computerSelection){
+    playerSelection = prompt('Choose your weapon! (Rock,Paper or Scissors only.)').toLowerCase();
+    computerSelection = getComputerChoice();
+    switch(computerSelection){
+        case 'rock':{
+            if (playerSelection == 'rock'){return "It's a Tie! - Rock vs. Rock"}
+            else if(playerSelection == 'paper'){return "You Win! - Paper beats Rock!"}
+            else if(playerSelection == 'scissors'){return "You Lose! - Rock beats Scissors. "} 
+            else{return "Error, not a valid input."}
+            return
+        };
+        break;
+        case 'paper':{
+            if(playerSelection == 'rock'){return 'You Lose! - Paper beats Rock.'}
+            else if(playerSelection == 'paper'){return "It's a Tie! - Paper vs. Paper"}
+            else if(playerSelection == 'scissors'){return "You Win! - Scissors cuts Paper!"}
+            else{return "Error, not a valid input."}
+            return
+        };
+        break;
+        case 'scissors':{
+            if(playerSelection == 'rock'){return 'You win! - Rock breaks Scissors!'}
+            else if(playerSelection == 'paper'){return 'You Lose! - Scissors cuts Paper.'}
+            else if(playerSelection == 'scissors'){return "It's a Tie! - Scissors vs. Scissors"}
+            else{return "Error, not a valid input."}
+            return
+        };
+        break
+        case null || undefined:{return 'Error!'};
+    }
+}
+
+console.log(round());
+
