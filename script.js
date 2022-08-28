de54e9d647faef29c01755e0310ff534c9716c52
AvihayR <avihayrabi@gmail.com>
2022-08-28 12:17:11 -0700

    Add game() func

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let computer = {
     choice: ['rock','paper','scissors']
 };
 
-//getComputerChoice - a func that generates a random number, multiplies it by computer.choice length, and floors it to a round num
+//getComputerChoice - a func that generates a random number, multiplies it by computer.choice length, and floors it to a round int
 function getComputerChoice(){
     let randomChoice = Math.floor(Math.random() * computer.choice.length);
-    console.log(computer.choice[randomChoice])
+    console.log(`Computer's choice: ${computer.choice[randomChoice]}`)
     return (computer.choice[randomChoice]);
 }
 
-//round - a func that takes two params - playerSelection & computerSelection,
+//playRound - a func that takes two params - playerSelection & computerSelection,
     //prompts an input pop-up window for the user then converts input into lowerCase. 
     //checks to see who's the winner and returns the string that declare if player won or lost.
-function round(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){
     playerSelection = prompt('Choose your weapon! (Rock,Paper or Scissors only.)').toLowerCase();
     computerSelection = getComputerChoice();
     switch(computerSelection){
@@ -34,7 +34,7 @@ function round(playerSelection,computerSelection){
         };
         break;
         case 'scissors':{
-            if(playerSelection == 'rock'){return 'You win! - Rock breaks Scissors!'}
+            if(playerSelection == 'rock'){return 'You Win! - Rock breaks Scissors!'}
             else if(playerSelection == 'paper'){return 'You Lose! - Scissors cuts Paper.'}
             else if(playerSelection == 'scissors'){return "It's a Tie! - Scissors vs. Scissors"}
             else{return "Error, not a valid input."}
@@ -45,5 +45,35 @@ function round(playerSelection,computerSelection){
     }
 }
 
-console.log(round());
+
+//game() func - first creates an object with two vars set to 0's,
+//then creates a for loop, with a counter to run 5 times, that for loop body contains:
+//var that holds the values that comes back from playRound() func, 
+//then determines which var in score object to score, checking via str.includes() built-in func, 
+//ex: if includes('win'){++score.player}
+
+
+function game(){
+    let score = {
+        player: 0 ,
+        computer: 0
+    };
+    for(let count = 0; count < 5; count++){
+
+        let round = playRound()
+        console.log(round)
+
+        if(round.includes('Win')){++score.player}
+        else if(round.includes('Lose')){++score.computer}
+        else if(round.includes('Tie')){++score.computer 
+            ++score.player};
+
+        console.log(score)
+    }
+    };
+
+
+game()
+
+
 
